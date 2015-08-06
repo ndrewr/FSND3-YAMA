ff68975c8bcfdec63e672213cf36d1fd15b3be12
uncleoptimus <uncle.optimus@gmail.com>
2015-08-05 19:37:37 -0700

    removed instance folder config ref in app __init__

--- yama/__init__.py
+++ yama/__init__.py
@@ -3,9 +3,10 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.github import GitHub
 
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+#app = Flask(__name__, instance_relative_config=True)
+#app.config.from_pyfile('config.py')
 
 db = SQLAlchemy(app)
 
