bbba7aca8185011dbefa0994faaaae8d3194d28b
uncleoptimus <uncle.optimus@gmail.com>
2015-08-06 21:37:06 -0700

    fixed a careless error in Form def that crashed the app on load

--- yama/forms.py
+++ yama/forms.py
@@ -10,4 +10,4 @@ from models import User, Category, Item
 class TitleDescriptionForm(Form):
   name = StringField('name', validators=[validators.Required(), validators.Length(min=2, max=100)])
   description = TextAreaField('description', validators=[validators.Length(max=300)], default='A new course.')
-  url = StringField('url', validators=[validators.Required(), validators.URL()], validators.Length(max=120))
+  url = StringField('url', validators=[validators.Required(), validators.URL(), validators.Length(max=120)])
