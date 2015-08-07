7114fedc33815a105e61b618fcac84e930d6c2ee
uncleoptimus <uncle.optimus@gmail.com>
2015-08-06 20:57:19 -0700

    fixed name def in Category model

--- yama/models.py
+++ yama/models.py
@@ -25,7 +25,7 @@ class User(db.Model):
 class Category(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.Integer, unique=True, nullable=False)
+    name = db.Column(db.String(250), unique=True, nullable=False)
     description = db.Column(db.String(250))
     courses = db.relationship("Item", backref='category', lazy='dynamic')
 
